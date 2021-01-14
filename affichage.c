860f965bbc0977c657f303663775d9aa17d1959b
Nadhem Maaloul <nadhemm1@gmail.com>
2021-01-14 21:41:23 +0100

    add simulation and small bug fixes

--- affichage.c
+++ affichage.c
@@ -1,4 +1,5 @@
 #include "initialisation.c"
+
 #define CLEAR "\033[H\033[J"
 
 
@@ -7,7 +8,7 @@ const int GREEN_COLOR = 1;
 const int ORANGE_COLOR = 2;
 const int RED_COLOR = 3;
 
-void printf_colored(char** text, int color){
+void printf_colored(char *text, int color) {
     if (color == GREEN_COLOR)
         printf("\033[38;5;10m%s\033[0m", text);
     else if (color == ORANGE_COLOR)
@@ -18,14 +19,14 @@ void printf_colored(char** text, int color){
         printf("%s", text);
 }
 
-void afficher_soignant(struct Bonhomme* bonhomme){
+void afficher_soignant(struct Bonhomme *bonhomme) {
     if (bonhomme->est_infecte)
         printf_colored("S", ORANGE_COLOR);
     else
         printf_colored("S", GREEN_COLOR);
 }
 
-void afficher_bonhomme(struct Bonhomme* bonhomme){
+void afficher_bonhomme(struct Bonhomme *bonhomme) {
     if (bonhomme->est_malade)
         printf_colored("M", RED_COLOR);
 
@@ -33,50 +34,56 @@ void afficher_bonhomme(struct Bonhomme* bonhomme){
         afficher_soignant(bonhomme);
 
     else {
-        char* output;
-        switch (bonhomme->direction){
+        char *output;
+        switch (bonhomme->direction) {
             case 0:
             case 7:
-                output = "\\"; break;
+                output = "\\";
+                break;
             case 1:
-                output = "^"; break;
+                output = "^";
+                break;
             case 2:
             case 5:
-                output = "/"; break;
+                output = "/";
+                break;
             case 3:
-                output = "<"; break;
+                output = "<";
+                break;
             case 4:
-                output = ">"; break;
+                output = ">";
+                break;
             case 6:
-                output = "v"; break;
+                output = "v";
+                break;
 
             default:
-                output = " "; break;
+                output = " ";
+                break;
         }
         printf_colored(output, bonhomme->est_infecte ? RED_COLOR : DEFAULT_COLOR);
-   }
+    }
 }
 
-void afficher_case(struct Case kase){ // case is a keyword
+void afficher_case(struct Case kase) { // case is a keyword
     if (kase.bonhomme != NULL)
         afficher_bonhomme(kase.bonhomme);
+    else if (kase.contaminee_pour_x_prochains_tours > 0)
+        printf_colored("V", RED_COLOR);
     else
-        if (kase.contaminee_pour_x_prochains_tours > 0)
-            printf_colored("V", RED_COLOR);
-        else
-            printf(" ");
+        printf(" ");
 }
 
-void afficher_matrice(struct Case matrice[N][M]){
-    for(int i=0; i<N; ++i){
-        for(int j=0; j<M; ++j)
+void afficher_matrice(struct Case matrice[N][M]) {
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < M; ++j)
             afficher_case(matrice[i][j]);
         printf("\n");
     }
 
 }
 
-void simuler_tour(int etape, struct Case matrice[N][M]){
+void simuler_tour(int etape, struct Case matrice[N][M]) {
     printf(CLEAR);
     printf("Etape numero %d:\n", etape);
     afficher_matrice(matrice);
---
+++ cmake-build-debug/CMakeFiles/c_project.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_project.dir/main.c.o"
+  "c_project"
+  "c_project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
